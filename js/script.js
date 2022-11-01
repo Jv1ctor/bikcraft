abd74be8253d7bbf663588a0e28316682648ba1a
Joao Victor <j.rodriguesbarretos@gmail.com>
2022-10-31 21:15:28 -0300

    js perguntas

--- js/script.js
+++ js/script.js
@@ -21,3 +21,22 @@ function ativarItem(item){
   }
 }
 parametros.forEach(ativarItem);
+
+// perguntas frequentes
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPergunta(itemP){
+  function ativoResposta(event){
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute("aria-controls");
+    const resposta = document.getElementById(controls);
+
+
+    resposta.classList.toggle("ativo-pergunta");
+    const ativo = resposta.classList.contains("ativo-pergunta")
+    pergunta.setAttribute("aria-expanded", ativo);
+  }
+
+  itemP.addEventListener("click",ativoResposta)
+}
+perguntas.forEach(ativarPergunta);
